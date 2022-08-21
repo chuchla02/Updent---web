c13cf208ca95b029e4b8dd6699da4d4dd01a22a8
chuchla02 <chuchla1337@gmail.com>
2022-08-22 01:04:34 +0200

    web

---
+++ script.js
@@ -0,0 +1,35 @@
+function reveal() { //Płynna animacja elementów podczas scrollowania
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+
+    if (elementTop < windowHeight) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+function createCookieEN() {
+  document.cookie = "language=en;expires=Thu, 01 Jan 2222 00:00:01 GMT;path=/";
+}
+
+function createCookiePL() {
+  document.cookie = "language=pl;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/";
+}
+
+/*function redirect() { //Przekierowanie  do strony angielskiej, jeżeli użytkownik posiada wersję językową przeglądarki innej niż PL.
+  const webLang = window.navigator.language;
+  currentPage = window.location.href;
+
+  if (webLang != 'pl' && currentPage.indexOf("en") == -1 && document.cookie.match('en') == null) {
+    window.location.replace("../en/index.html")
+  }
+
+}
+
+window.addEventListener("load", redirect);*/

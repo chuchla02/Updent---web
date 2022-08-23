1feb0041118fa60d9c6e9a058f3bfebe20b5006d
chuchla02 <chuchla1337@gmail.com>
2022-08-23 15:03:28 +0200

    web

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 window.addEventListener("scroll", () => {
 
   const reveals = document.querySelectorAll(".reveal");
-
   for (let i = 0; i < reveals.length; i++) {
     let windowHeight = window.innerHeight;
     let elementTop = reveals[i].getBoundingClientRect().top;
@@ -12,40 +11,53 @@ window.addEventListener("scroll", () => {
     } else {
       reveals[i].classList.remove("active");
     }
+
   }
 })
 
-
 function createCookieEN() {
-  document.cookie = "language=en;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/";
+  document.cookie = "language=en;expires=Thu, 01 Jan 2022 00:00:01 GMT;path=/";
 }
-
 function createCookiePL() {
   document.cookie = "language=pl;expires=Thu, 01 Jan 2222 00:00:01 GMT;path=/";
 }
 
 
-
-window.addEventListener("load", () => {
-  
+function redirect() {
   const windowWidth = window.innerWidth;
   const webLang = window.navigator.language;
   currentPage = window.location.href;
+  console.log(document.cookie.match('pl'))
 
- if (windowWidth > '900') {
-  if (webLang != 'pl'  && currentPage.indexOf("/en/") == -1 && document.cookie.match('en') == null) {
+ {
+  if (webLang != 'pl'  && currentPage.indexOf("/en/") == -1 && document.cookie.match('pl') == null) {
     window.location.replace("../en/index.html")
   }
+  
+}
+}
+
+function backWithCookie() {
+  const cookiePL = document.cookie.match('pl')
+  const cookieEN = document.cookie.match('en')
+
+  if(cookiePL == 'pl') {
+    return 0;
+  } else if (cookieEN == 'en') {
+    window.location.replace("../en/index.html")
   }
-});
+}
+
+window.addEventListener("load", redirect)
 
 
 window.addEventListener("scroll", () => {
   const button = document.getElementById('backbutton');
   const y = window.scrollY;
+  const showHeight = 110;
 
 
-  if (y > 110)
+  if (y > showHeight)
   {
    button.classList.remove("hidden"); 
    button.classList.add("remove");
